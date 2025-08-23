23b319f2b56dbe202bfbc5d12e502a39960ff5db
Vaibhav-G-Parmar <vgparmar@myseneca.ca>
2025-08-23 13:02:01 -0400

    adds React Component

---
+++ R8-react-components/src/List.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function List(){
+    return(
+        <>
+            <ul>
+                <li>Pizza</li>
+                <li>Burger</li>
+                <li>Sandwich</li>
+            </ul>
+        </>
+    )
+}
+
+export default List;
