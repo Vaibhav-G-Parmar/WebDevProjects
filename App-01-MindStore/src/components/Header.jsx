dc193e9c8563275385604dbd3cc2f4651b41221f
Vaibhav-G-Parmar <vaibhavgirish2093@gmail.com>
2025-08-23 21:05:18 -0400

    adds header, footer and note components

---
+++ App-01-MindStore/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header(){
+    return(
+        <div>
+            <header><h1>MindStore</h1></header>
+        </div>
+    )
+}
+
+export default Header;
---
+++ App-01-MindStore/src/components/Note.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Note = () => {
+  return (
+    <div className='note'>
+      <h1>Title of the Note</h1>
+      <p>Contents of the Note</p>
+    </div>
+  )
+}
+
+export default Note;
+
+
