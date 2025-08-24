3f466dc93cf43e67e171af29e0f35537089f8364
Vaibhav-G-Parmar <vaibhavgirish2093@gmail.com>
2025-08-24 11:26:57 -0400

    adds Mapping Components

---
+++ R013-mapping-components/src/contacts.js
@@ -0,0 +1,25 @@
+const contacts = [
+  {
+    id: 1,
+    name: "Hari",
+    imgURL: "https://www.pelago.com/img/countries/new-zealand/0811-1600_newzealand.jpg",
+    phone: "+123 456 789",
+    email: "hari@dham.com"
+  },
+  {
+    id: 2,
+    name: "Das",
+    imgURL: "https://www.we12travel.com/blog/wp-content/uploads/2016/11/Oevers-van-Lake-Hauroko-1-1140x855.jpg.webp",
+    phone: "+987 654 321",
+    email: "das@nowhere.com"
+  },
+  {
+    id: 3,
+    name: "Saviour",
+    imgURL: "https://internationalliving.com/_next/image/?url=https%3A%2F%2Fimages.ctfassets.net%2Fwv75stsetqy3%2F6x7tcvxp9l1fLEeOgQybAC%2Fd9454196dfcd3a380507eaea1a12e7de%2FNew_Zealand.jpg%3Fq%3D60%26fit%3Dfill%26fm%3Dwebp&w=3840&q=60",
+    phone: "+918 372 574",
+    email: "gmail@saviour.com"
+  }
+];
+
+export default contacts; 
